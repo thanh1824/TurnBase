96ce9aa9f85a9e59375ca662ee3178c784db15f5
unknown <soict.nguyenthanh@gmail.com>
2024-10-26 15:28:10 +0700

    2

---
+++ Assets/Scripts/RotateTowardsCenter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTowardsCenter : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.flipX = true; // Lật hero về bên trái
+        }
+        else
+        {
+            Debug.LogError("SpriteRenderer chưa được gán cho " + gameObject.name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
